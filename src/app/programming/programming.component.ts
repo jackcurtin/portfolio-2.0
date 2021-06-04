d538fed95af0da06c9e398917f96809854a386b5
Jack Curtin <jackcurtin94@gmail.com>
2021-06-04 13:40:47 -0500

    set up programming list to read from ts file

--- src/app/programming/programming.component.ts
+++ src/app/programming/programming.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {PROGRAMMING} from "./programming";
 
 @Component({
   selector: 'app-programming',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./programming.component.css']
 })
 export class ProgrammingComponent implements OnInit {
+  programList = PROGRAMMING;
 
   constructor() { }
 
---
+++ src/app/programming/programming.ts
@@ -0,0 +1,11 @@
+export const PROGRAMMING = [
+  {
+    id: 1,
+    title: 'next groove',
+    link: 'https://next-groove.herokuapp.com/welcome',
+    tech: 'angular, springboot framework, & postgres',
+    tagline: 'algorithmic playlisting for vinyl enthusiasts',
+    caveat: 'please give the site a few minutes to load information from the database',
+    icon: 'https://static.thenounproject.com/png/934821-200.png'
+  }
+]
